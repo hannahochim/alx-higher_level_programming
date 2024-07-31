eb309422268265a337a5274835ccbb082b739272
hannahochim <hannahochim@gmail.com>
2024-07-31 06:50:40 +0000

    Print movie count

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,11 +1,25 @@
 #!/usr/bin/node
-const request = require('request');
-const end = process.argv[2];
+// A script that prints the number of movies where the character Wedge Antilles is present
 
-request('https://swapi-api.hbtn.io/api/films/' + end, function (error, respones, body) {
+const args = process.argv;
+const reqURL = args[2];
+const request = require('request');
+request(reqURL, function (error, response, body) {
   if (error) {
-    console.log(error);
+    console.log('error:', error); // Print the error if one occurred
   } else {
-    console.log(JSON.parse(body).title);
+    const jso = JSON.parse(body);
+    const results = jso.results;
+    let count = 0;
+    for (let i = 0; i < results.length; i++) {
+      const chars = (results[i].characters);
+      for (let j = 0; j < chars.length; j++) {
+        const check18 = chars[j].endsWith('18/');
+        if (check18) {
+          count++;
+        }
+      }
+    }
+    console.log(count);
   }
 });
